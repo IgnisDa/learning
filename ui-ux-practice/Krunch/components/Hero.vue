fd953b6d9e5ae663da6787e2688798e6fc0cf2bd
IgnisDa <ignisda2001@gmail.com>
2021-01-27 13:59:48 +0550

    feat(hero-bg.jpg, Hero.vue, HeroNavbar.vue): add hero page

---
+++ ui-ux-practice/Krunch/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="hero-background">
+    <div class="max-w-6xl mx-auto container pt-5">
+      <HeroNavbar />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#hero-background {
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),
+    url('~assets/images/hero-bg.jpg');
+  height: 770px;
+  @apply bg-local bg-center bg-no-repeat;
+}
+</style>
---
+++ ui-ux-practice/Krunch/components/HeroNavbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-transparent">
+    <div class="flex justify-between items-center py-10">
+      <div class="font-bold text-white text-3xl">Krunch</div>
+      <div class="flex uppercase font-bold text-gray-50 text-sm">
+        <a
+          href="#"
+          class="ml-10 transition duration-300 text-blue-500 hover:text-blue-400"
+        >
+          Home
+        </a>
+        <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          About
+        </a>
+        <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          Portfolio
+        </a>
+        <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          Services
+        </a>
+        <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          Team
+        </a>
+        <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          Blog
+        </a>
+        <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          Contact
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
