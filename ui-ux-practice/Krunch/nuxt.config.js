6e582ea9fe43f402f42979afcd82f474bd709df0
IgnisDa <ignisda2001@gmail.com>
2021-01-29 23:35:59 +0550

    feat(nuxt.config.js): add description

--- ui-ux-practice/Krunch/nuxt.config.js
+++ ui-ux-practice/Krunch/nuxt.config.js
@@ -5,7 +5,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Replication of http://krunch.webflow.io/ using tailwindcss and nuxtjs',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
