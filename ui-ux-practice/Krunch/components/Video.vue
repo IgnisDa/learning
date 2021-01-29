f464806cbe453df82205011027a885950a4a03ec
IgnisDa <ignisda2001@gmail.com>
2021-01-29 23:25:53 +0550

    feat(Video.vue): complete video section component

--- ui-ux-practice/Krunch/components/Video.vue
+++ ui-ux-practice/Krunch/components/Video.vue
@@ -1,50 +1,39 @@
 <template>
   <div
-    id="team"
+    id="video"
     class="mt-20 flex flex-col md:max-w-7xl mx-auto justify-center items-center"
   >
-    <div class="mx-auto text-center">
-      <div class="uppercase my-1 font-semibold text-3xl text-gray-700">
-        Awesome Team
-      </div>
-      <div class="my-3 text-sm text-gray-700 italic md:text-lg">
-        Setibulum rutrum quam vitae fringilla tincidunt
-      </div>
-    </div>
-    <div class="w-20 h-0.5 mx-auto my-4 bg-blue-400"></div>
-    <div class="flex flex-wrap mx-3 md:w-5/6 lg:w-11/12">
+    <div>
+      <FontAwesomeIcon
+        class="h-20 w-20 fill-current text-white mx-auto cursor-pointer"
+        :icon="['fas', 'play-circle']"
+      />
       <div
-        v-for="(person, index) in people"
-        :key="index"
-        class="w-full my-4 md:w-1/2 lg:w-1/4 md:p-3"
+        class="mx-auto text-center text-lg text-white w-9/12 leading-loose italic"
       >
         <div>
-          <img
-            :src="require(`~/assets/images/team-${index + 1}.jpg`)"
-            :alt="`team-${index + 1}.jpg`"
-            class="h-full w-full object-cover"
-          />
-          <div class="text-center text-gray-800 mt-3 font-bold">
-            {{ person.name }}
-          </div>
-          <div class="text-center text-gray-600 my-1">
-            {{ person.position }}
-          </div>
+          Aenean commodo ligula eget dolor sociis natoque penatibus et magnis
         </div>
+        <div>dis parturient montes, nascetur ridiculus consectetur muse.</div>
       </div>
     </div>
+    <button
+      class="uppercase mt-10 bg-blue-500 my-2 md:mx-3 p-3 text-gray-50 text-sm font-bold tracking-wide px-8 rounded-md hover:bg-gray-800 transition duration-200 ease-in"
+    >
+      Download Now
+    </button>
   </div>
 </template>
 
-<script>
-export default {
-  data: () => ({
-    people: [
-      { name: 'MICHAEL THOMAS', position: 'Creative Director' },
-      { name: 'ALEXA MEYER', position: 'Graphic Designer' },
-      { name: 'JEFFREY STEWARD', position: 'Web Designer' },
-      { name: 'BRUCE MOORE', position: 'Web Designer' },
-    ],
-  }),
+<style scoped>
+#video {
+  min-height: 470px;
+  background-image: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url('~assets/images/video-bg.jpg');
+  @apply bg-local bg-center bg-no-repeat bg-cover;
 }
-</script>
+</style>
