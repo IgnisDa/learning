c57c6009d9a9a265dbb22191fbd17f2f5c877012
IgnisDa <ignisda2001@gmail.com>
2021-01-29 10:12:51 +0550

    fix: text size

--- ui-ux-practice/Krunch/components/HeroNavbar.vue
+++ ui-ux-practice/Krunch/components/HeroNavbar.vue
@@ -2,7 +2,9 @@
   <div class="mt-10 md:mt-14">
     <div class="bg-transparent md:max-w-6xl mx-auto">
       <div class="flex justify-between items-center mx-6 md:mx-0">
-        <div class="font-bold text-white text-3xl">Krunch</div>
+        <NuxtLink :to="{ name: 'index' }">
+          <div class="font-bold text-white text-3xl">Krunch</div>
+        </NuxtLink>
         <div class="transition duration-300 hover:bg-gray-900 md:hidden p-2">
           <FontAwesomeIcon
             class="h-6 w-6 fill-current transition duration-200 ease-in text-white"
@@ -10,28 +12,46 @@
           />
         </div>
         <div class="uppercase font-bold text-gray-50 text-sm hidden sm:block">
-          <a
-            href="#"
+          <NuxtLink
+            :to="{ name: 'index' }"
             class="ml-10 transition duration-300 text-blue-500 hover:text-blue-400"
           >
             Home
-          </a>
-          <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          </NuxtLink>
+          <a
+            href="#about"
+            class="ml-10 transition duration-300 hover:text-blue-400"
+          >
             About
           </a>
-          <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          <a
+            href="#portfolio"
+            class="ml-10 transition duration-300 hover:text-blue-400"
+          >
             Portfolio
           </a>
-          <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          <a
+            href="#services"
+            class="ml-10 transition duration-300 hover:text-blue-400"
+          >
             Services
           </a>
-          <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          <a
+            href="#team"
+            class="ml-10 transition duration-300 hover:text-blue-400"
+          >
             Team
           </a>
-          <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          <a
+            href="#blog"
+            class="ml-10 transition duration-300 hover:text-blue-400"
+          >
             Blog
           </a>
-          <a href="#" class="ml-10 transition duration-300 hover:text-blue-400">
+          <a
+            href="#contact"
+            class="ml-10 transition duration-300 hover:text-blue-400"
+          >
             Contact
           </a>
         </div>
