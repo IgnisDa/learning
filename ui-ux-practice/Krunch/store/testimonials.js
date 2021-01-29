a928c8da621cece9aa7d92f76c227b29ffc4807a
IgnisDa <ignisda2001@gmail.com>
2021-01-29 22:48:01 +0550

    feat(testimonials.js): vuex store for storage of testimonial data

---
+++ ui-ux-practice/Krunch/store/testimonials.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+  testimonials: [
+    {
+      text:
+        'It really saves me time and effort. web design is exactly what our business has been lacking. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem risus tristique posuere.',
+      author: 'Walter Ward',
+    },
+    {
+      text:
+        'It really saves me time and effort. web design is exactly what our business has been lacking. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem risus tristique posuere.',
+      author: 'Walter Ward',
+    },
+    {
+      text:
+        'It really saves me time and effort. web design is exactly what our business has been lacking. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem risus tristique posuere.',
+      author: 'Walter Ward',
+    },
+  ],
+})
