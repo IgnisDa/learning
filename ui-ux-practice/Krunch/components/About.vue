4c6039524844677919c72422724cb53875375e1d
IgnisDa <ignisda2001@gmail.com>
2021-01-29 09:37:05 +0550

    feat(about.jpg, About.vue): complete about section

--- ui-ux-practice/Krunch/components/About.vue
+++ ui-ux-practice/Krunch/components/About.vue
@@ -1,5 +1,36 @@
 <template>
-  <div id="#about" class="mt-10">
-    <div class="uppercase font-bold text-2xl text-gray-800">About Us</div>
+  <div
+    id="#about"
+    class="mt-20 flex flex-col md:max-w-7xl mx-auto justify-center items-center"
+  >
+    <div class="mx-auto text-center">
+      <div class="uppercase my-1 font-semibold text-3xl text-gray-700">
+        About Us
+      </div>
+      <div class="my-3 text-sm text-gray-700 italic">
+        Setibulum rutrum quam vitae fringilla tincidunt
+      </div>
+    </div>
+    <div class="w-20 h-0.5 mx-auto my-4 bg-blue-400"></div>
+    <div class="flex flex-wrap mx-3">
+      <div class="w-full md:w-1/2 text-gray-600 my-auto md:p-3">
+        <div>
+          Lorem ipsum dolor sitamet, consectetur adipisicing elit, seddo eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quisus nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
+        </div>
+        <div class="mt-3">
+          Excepteur sint occaecat cupidatat none proident, sunt in culpa qui
+          officia deserunt mollit anim id est laborum voluptate dolor.
+        </div>
+      </div>
+      <div class="w-full md:w-1/2 md:p-3">
+        <img src="~/assets/images/about.jpg" alt="about image" />
+      </div>
+    </div>
   </div>
 </template>
