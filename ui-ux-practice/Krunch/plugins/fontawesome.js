c5dde783d20095534cd3cad7a2edaaa4e9165f6e
IgnisDa <ignisda2001@gmail.com>
2021-01-29 22:48:51 +0550

    feat(fontawesome.js): add icons for project

--- ui-ux-practice/Krunch/plugins/fontawesome.js
+++ ui-ux-practice/Krunch/plugins/fontawesome.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faChevronDown, faBars } from '@fortawesome/free-solid-svg-icons'
+import {
+  faChevronDown,
+  faBars,
+  faCode,
+  faDesktop,
+  faCamera,
+  faMagic,
+  faGlobeAsia,
+} from '@fortawesome/free-solid-svg-icons'
+import { faAndroid } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faChevronDown, faBars)
+library.add(
+  faChevronDown,
+  faCode,
+  faBars,
+  faDesktop,
+  faAndroid,
+  faGlobeAsia,
+  faCamera,
+  faMagic
+)
 
 // Register the component globally
 Vue.component('FontAwesomeIcon', FontAwesomeIcon)
