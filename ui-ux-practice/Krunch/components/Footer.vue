f5418b5b4fab0ab07ea28b969d557c23fd94583c
IgnisDa <ignisda2001@gmail.com>
2021-01-30 10:43:30 +0550

    feat(Footer.vue): add component to handle footer

---
+++ ui-ux-practice/Krunch/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="footer" class="p-8">
+    <div
+      class="flex justify-center w-1/3 mx-auto text-white uppercase tracking-tighter text-xs sm:text-sm mb-5"
+    >
+      <a href="#" class="mx-2">Facebook</a>
+      <a href="#" class="mx-2">Twitter</a>
+      <a href="#" class="mx-2">Instagram</a>
+      <a href="#" class="mx-2">Google+</a>
+    </div>
+    <div class="text-center text-sm text-gray-400 leading-snug uppercase">
+      © 2016 Krunch Landing Page, Designed By
+      <a
+        target="_blank"
+        href="https://github.com/IgnisDa/"
+        class="text-blue-500"
+        >IgnisDa</a
+      >
+      &amp; Tailwind-ed By
+      <a
+        target="_blank"
+        href="https://github.com/IgnisDa/"
+        class="text-blue-500"
+        >IgnisDa</a
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#footer {
+  background-color: #222;
+}
+</style>
