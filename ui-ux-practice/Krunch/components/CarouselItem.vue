93c55a32aa142981a6a45e566aad692ef9638a60
IgnisDa <ignisda2001@gmail.com>
2021-01-31 20:44:27 +0550

    feat(CarouselItem.vue): add component to handle a carousel items

--- ui-ux-practice/Krunch/components/CarouselItem.vue
+++ ui-ux-practice/Krunch/components/CarouselItem.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    <div>Testimonial</div>
+  <div v-if="active">
+    <div class="flex-grow flex flex-col justify-around">
+      <div class="mt-6">
+        <div
+          class="text-center text-white sm:w-1/2 sm:text-lg mx-auto w-2/3 leading-relaxed italic"
+        >
+          {{ testimonial.text }}
+        </div>
+        <div
+          class="mt-5 uppercase font-semibold text-blue-500 mx-auto text-center"
+        >
+          {{ testimonial.author }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    testimonial: {
+      type: Object,
+      default: () => {
+        return {}
+      },
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
