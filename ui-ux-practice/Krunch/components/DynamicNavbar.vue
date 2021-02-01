11df9b62fa344a44acea743fcfaf5a3ec3c08604
IgnisDa <ignisda2001@gmail.com>
2021-02-01 22:42:10 +0550

    feat(DynamicNavbar.vue): add component to handle dynamic navbar

---
+++ ui-ux-practice/Krunch/components/DynamicNavbar.vue
@@ -0,0 +1,108 @@
+<template>
+  <transition name="navbar">
+    <div
+      v-show="showNavbar"
+      id="navbar"
+      class="bg-black py-2 md:py-6 md:px-10 bg-opacity-80 w-full fixed"
+    >
+      <div class="md:max-w-6xl mx-auto">
+        <div class="flex justify-between items-center mx-6 md:mx-0">
+          <NuxtLink :to="{ name: 'index' }">
+            <div class="font-bold text-white text-3xl">Krunch</div>
+          </NuxtLink>
+          <div class="transition duration-300 hover:bg-gray-900 md:hidden p-2">
+            <FontAwesomeIcon
+              class="h-6 w-6 fill-current transition duration-200 ease-in text-white"
+              :icon="['fas', 'bars']"
+            />
+          </div>
+          <div class="uppercase font-bold text-gray-50 text-sm hidden sm:block">
+            <NuxtLink
+              :to="{ name: 'index' }"
+              class="ml-10 transition duration-300 text-blue-500 hover:text-blue-400"
+            >
+              Home
+            </NuxtLink>
+            <a
+              href="#about"
+              class="ml-10 transition duration-300 hover:text-blue-400"
+            >
+              About
+            </a>
+            <a
+              href="#portfolio"
+              class="ml-10 transition duration-300 hover:text-blue-400"
+            >
+              Portfolio
+            </a>
+            <a
+              href="#services"
+              class="ml-10 transition duration-300 hover:text-blue-400"
+            >
+              Services
+            </a>
+            <a
+              href="#team"
+              class="ml-10 transition duration-300 hover:text-blue-400"
+            >
+              Team
+            </a>
+            <a
+              href="#blog"
+              class="ml-10 transition duration-300 hover:text-blue-400"
+            >
+              Blog
+            </a>
+            <a
+              href="#contact"
+              class="ml-10 transition duration-300 hover:text-blue-400"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    showNavbar: false,
+  }),
+  mounted() {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll() {
+      const heroOffsetHeight = document.getElementById('hero').offsetHeight
+      const currentScrollPos = window.pageYOffset
+      if (currentScrollPos > heroOffsetHeight) {
+        this.showNavbar = true
+      } else {
+        this.showNavbar = false
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.navbar-enter,
+.navbar-leave-to {
+  visibility: hidden;
+  height: 0;
+  margin: 0;
+  padding: 0;
+  opacity: 0;
+}
+
+.navbar-enter-active,
+.navbar-leave-active {
+  transition: all 0.2s;
+}
+</style>
