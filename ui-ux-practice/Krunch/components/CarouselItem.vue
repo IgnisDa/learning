cde78da996a5f47ec28ca80c8bb1f9ba52489b0a
IgnisDa <ignisda2001@gmail.com>
2021-02-02 22:30:02 +0550

    feat(CarouselItem.vue): remove transition tag

--- ui-ux-practice/Krunch/components/CarouselItem.vue
+++ ui-ux-practice/Krunch/components/CarouselItem.vue
@@ -1,21 +1,19 @@
 <template>
   <div v-if="active">
-    <transition tag="div" name="fade">
-      <div class="flex-grow flex flex-col justify-around">
-        <div class="mt-6">
-          <div
-            class="text-center text-white sm:w-1/2 sm:text-lg mx-auto w-2/3 leading-relaxed italic"
-          >
-            {{ testimonial.text }}
-          </div>
-          <div
-            class="mt-5 uppercase font-semibold text-blue-500 mx-auto text-center"
-          >
-            {{ testimonial.author }}
-          </div>
+    <div class="flex-grow flex flex-col justify-around">
+      <div class="mt-6">
+        <div
+          class="text-center text-white sm:w-1/2 sm:text-lg mx-auto w-2/3 leading-relaxed italic"
+        >
+          {{ testimonial.text }}
+        </div>
+        <div
+          class="mt-5 uppercase font-semibold text-blue-500 mx-auto text-center"
+        >
+          {{ testimonial.author }}
         </div>
       </div>
-    </transition>
+    </div>
   </div>
 </template>
 
@@ -37,17 +35,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.fade-leave-active,
-.fade-enter-active {
-  transition: 1s;
-  color: bisque;
-}
-.fade-enter {
-  transform: translate(100%, 0);
-}
-.fade-leave-to {
-  transform: translate(-100%, 0);
-}
-</style>
