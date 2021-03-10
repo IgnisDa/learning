c169e426deb022b4aead3dce8946f43a5b8edd94
IgnisDa <ignisda2001@gmail.com>
2021-03-10 22:06:44 +0550

    feat(fontawesome.js): add icons for channel bar

--- ui-ux-practice/discord-clone/plugins/fontawesome.js
+++ ui-ux-practice/discord-clone/plugins/fontawesome.js
@@ -6,15 +6,30 @@ import {
   faFolder,
   faPlus,
   faCompass,
+  faHeadphones,
+  faCog,
+  faMicrophone,
+  faUserFriends,
 } from '@fortawesome/free-solid-svg-icons'
-import { faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { faDiscord, faAlgolia } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faDownload, faFolder, faPlus, faCompass, faDiscord)
+library.add(
+  faDownload,
+  faFolder,
+  faPlus,
+  faCompass,
+  faHeadphones,
+  faCog,
+  faMicrophone,
+  faUserFriends,
+  faDiscord,
+  faAlgolia
+)
 
 // Register the component globally
 Vue.component('FontAwesomeIcon', FontAwesomeIcon)
