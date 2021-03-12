d120efe5ec59fc34c73f54b372bbf3e6b59e3328
IgnisDa <ignisda2001@gmail.com>
2021-03-12 11:11:13 +0550

    feat(InformationBar.vue): add component to display server info

---
+++ ui-ux-practice/discord-clone/components/InformationBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="absolute inset-x-0 flex items-center justify-center flex-grow h-12 border-b border-black bg-light-black border-opacity-70"
+  >
+    <div class="flex items-center mx-3 space-x-2">
+      <FontAwesomeIcon
+        class="w-5 h-5 text-gray-500 fill-current"
+        :icon="['fas', 'hashtag']"
+      ></FontAwesomeIcon>
+      <div class="font-bold text-gray-50">help</div>
+    </div>
+    <div
+      class="flex-grow pl-4 mx-2 text-sm tracking-tighter text-gray-300 border-l border-gray-600"
+    >
+      Help with Tailwind CSS and build process integration.
+    </div>
+    <div class="flex items-center mx-3 space-x-5">
+      <FontAwesomeIcon
+        class="w-5 h-5 text-gray-400 cursor-pointer fill-current hover:text-gray-200"
+        :icon="['fas', 'bell']"
+      ></FontAwesomeIcon>
+      <FontAwesomeIcon
+        class="w-5 h-5 text-gray-200 transform rotate-45 cursor-pointer fill-current hover:text-gray-200"
+        :icon="['fas', 'thumbtack']"
+      ></FontAwesomeIcon>
+      <FontAwesomeIcon
+        class="w-5 h-5 text-gray-400 cursor-pointer fill-current hover:text-gray-200"
+        :icon="['fas', 'users']"
+      ></FontAwesomeIcon>
+    </div>
+  </div>
+</template>
