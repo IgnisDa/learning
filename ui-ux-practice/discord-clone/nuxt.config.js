7e3936fc9abf34a1919a88166d8c576928c86545
IgnisDa <ignisda2001@gmail.com>
2021-03-17 17:43:25 +0550

    feat(nuxt.config.js): add description for discord-clone

--- ui-ux-practice/discord-clone/nuxt.config.js
+++ ui-ux-practice/discord-clone/nuxt.config.js
@@ -5,7 +5,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'A discord clone that I built to polish my UI/UX skills',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
