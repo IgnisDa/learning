110b771d2ac33b20ab6e4f65364bf27e17a43bc6
IgnisDa <ignisda2001@gmail.com>
2021-03-18 09:18:57 +0550

    feat(Info.vue): unread message indication grows on hover

--- ui-ux-practice/discord-clone/components/server/Info.vue
+++ ui-ux-practice/discord-clone/components/server/Info.vue
@@ -6,21 +6,26 @@
       :class="{ 'h-5': hovered }"
     ></div>
     <div class="flex items-center justify-center flex-grow w-full">
-      <div class="relative">
+      <div
+        class="relative"
+        @mouseover="handleMouseOver()"
+        @mouseleave="handleMouseLeave()"
+      >
         <img
           v-if="noImage !== true"
           class="w-12 h-12 duration-300 cursor-pointer hover:rounded-xl transition-border-radius rounded-[100px]"
           :src="`https://picsum.photos/seed/${getRandomString()}/300`"
           :alt="`${serverName}-icon`"
           loading="lazy"
-          @mouseover="hovered = true"
-          @mouseleave="hovered = false"
         />
         <ServerInfoInitials
           v-else
           class="w-12 h-12 text-lg"
           :server-name="serverName"
         ></ServerInfoInitials>
+        <!-- <div v-if="showTooltip" class="absolute inset-0 flex left-12">
+          <div class="z-[9999] my-auto bg-red-400 border-8">Hello</div>
+        </div> -->
         <div
           v-if="notifications !== 0"
           class="absolute bottom-0 right-0 flex items-center justify-center w-6 h-6 text-xs font-semibold text-white bg-red-600 border-4 rounded-full cursor-pointer pointer-events-none border-almost-black"
@@ -56,8 +61,17 @@ export default {
   },
   data: () => ({
     hovered: false,
+    showTooltip: false,
   }),
   methods: {
+    handleMouseOver() {
+      this.hovered = true
+      this.showTooltip = true
+    },
+    handleMouseLeave() {
+      this.hovered = false
+      this.showTooltip = false
+    },
     getInitials(string) {
       const names = string.split(' ')
       let initials = names[0].substring(0, 1).toUpperCase()
