1b2979c44e256c7067cd83ec8a9e4c5784e5f721
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-03-24 22:02:13 +0550

    feat(ui-ux-practice/spotify-clone): add practice project

---
+++ ui-ux-practice/spotify-clone/README.md
@@ -0,0 +1,3 @@
+# spotify-clone
+
+This is my attempt to rebuild the [spotify](https://open.spotify.com/) website.
---
+++ ui-ux-practice/spotify-clone/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container text-white m-4 rounded-lg bg-[green] text-7xl">
+    Hello World!
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
