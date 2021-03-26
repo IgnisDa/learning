c415258d6f798d594cb9736b8ad065b7f0a12bdb
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-03-26 11:29:59 +0550

    feat(ListeningOnBar.vue): display this component conditionally

--- ui-ux-practice/spotify-clone/components/ListeningOnBar.vue
+++ ui-ux-practice/spotify-clone/components/ListeningOnBar.vue
@@ -1,3 +1,13 @@
 <template>
-  <div id="listening-on-bar" class="h-6 bg-spotify-green">NavBar</div>
+  <div
+    v-show="listeningOnOtherDevice"
+    id="listening-on-bar"
+    class="h-6 bg-spotify-green"
+  >
+    NavBar
+  </div>
 </template>
+
+<script>
+export default { data: () => ({ listeningOnOtherDevice: false }) }
+</script>
