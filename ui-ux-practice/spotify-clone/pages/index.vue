74175b3d178f7e6421ed3f3692e457b1a18f4fc8
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-03-27 13:03:09 +0550

    feat(index.vue): add basic layout

--- ui-ux-practice/spotify-clone/pages/index.vue
+++ ui-ux-practice/spotify-clone/pages/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="text-white w-full h-full bg-[green] text-7xl">Hello World!</div>
+  <div class="flex flex-col w-full px-6 mb-6 overflow-y-auto">
+    <div class="flex-none">
+      <div v-for="i in 20" :id="i" :key="i">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cum ullam
+        nihil est omnis, enim blanditiis rem eum cumque, numquam quo ea vero
+        fuga minus nemo voluptate similique at fugit! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Asperiores quis a similique ratione autem
+        provident quas, consequatur facere. Doloribus quidem, quia cumque
+        repellendus non, nostrum officia numquam similique, atque quo expedita
+        saepe. Quisquam alias, asperiores aut reiciendis temporibus ea aperiam
+        iste tempora dolorum repellendus magni esse fugit non soluta cum
+        laboriosam quidem numquam sint. Mollitia suscipit exercitationem porro,
+        nemo officia sequi molestiae quibusdam fuga labore ratione voluptatum
+        architecto, unde ut soluta debitis harum aperiam aliquid, sed ad quasi.
+        Doloribus, ratione unde. Ratione omnis voluptatum suscipit blanditiis
+        consequuntur ad temporibus fuga maiores dolor architecto, dolorem
+        assumenda incidunt quas, veniam esse odit!
+        <span>hello hello</span>666666 <span>hello hello</span>666666
+        <span>hello hello</span>666666 <span>hello hello</span>666666
+        <span>hello hello</span>666666 <span>hello hello</span>666666
+        <span>hello hello</span>666666 <span>hello hello</span>666666
+        <span>hello hello</span>666666 <span>hello hello</span>666666
+        <span>hello hello</span>666666 <span>hello hello</span>666666
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
