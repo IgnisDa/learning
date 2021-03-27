4304b637d4ed171728e30f64925967b5faf13bf5
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-03-27 17:35:55 +0550

    feat(PlayIcon.vue): icon to be displayed on music cards

---
+++ ui-ux-practice/spotify-clone/components/card/PlayIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="flex items-center justify-center flex-none transition duration-200 transform"
+    :class="!hovered ? 'opacity-0 translate-y-2' : '-translate-y-0'"
+  >
+    <div
+      class="p-2 transition-transform transform rounded-full shadow-2xl bg-spotify-green hover:scale-105"
+    >
+      <ig-icon
+        name="play"
+        size="lg"
+        class="text-white transform translate-x-0.5"
+      ></ig-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    hovered: {
+      default: false,
+      type: Boolean,
+    },
+  },
+}
+</script>
