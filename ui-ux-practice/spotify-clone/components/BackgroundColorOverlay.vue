610a435b3986e33c846ecd86ae9de62629b8c619
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-03-28 16:06:40 +0550

    feat(BackgroundColorOverlay.vue): color change on hover

--- ui-ux-practice/spotify-clone/components/BackgroundColorOverlay.vue
+++ ui-ux-practice/spotify-clone/components/BackgroundColorOverlay.vue
@@ -1,6 +1,30 @@
 <template>
   <div
     id="background-color-overlay"
-    class="inset-0 z-[-1] absolute bg-gradient-to-t from-dark-black to-black"
+    class="inset-0 z-[-1] absolute transition-colors duration-1000"
+    :class="getBackgroundColor"
   ></div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      backgroundColorState: (state) => state.background.backgroundColor,
+    }),
+    getBackgroundColor() {
+      switch (this.backgroundColorState) {
+        case 'red':
+          return 'bg-red-900'
+        case 'green':
+          return 'bg-green-900'
+        case 'blue':
+          return 'bg-gray-900'
+        default:
+          return 'bg-light-black'
+      }
+    },
+  },
+}
+</script>
