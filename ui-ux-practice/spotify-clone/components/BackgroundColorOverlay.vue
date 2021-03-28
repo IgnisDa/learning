f7fc11a3b430fa7641a6b7a0b6782ea38198ab38
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-03-28 10:57:17 +0550

    feat(BackgroundColorOverlay.vue): add gradient to bg

--- ui-ux-practice/spotify-clone/components/BackgroundColorOverlay.vue
+++ ui-ux-practice/spotify-clone/components/BackgroundColorOverlay.vue
@@ -1,3 +1,6 @@
 <template>
-  <div id="background-color-overlay" class="inset-0 z-[-1] absolute"></div>
+  <div
+    id="background-color-overlay"
+    class="inset-0 z-[-1] absolute bg-gradient-to-t from-dark-black to-black"
+  ></div>
 </template>
