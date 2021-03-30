a92243a68d4264e333fb30095ebd6957b04050c3
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-03-30 11:14:56 +0550

    feat(lib.rs, mod.rs, test.rs, main.rs): refactor file to new module

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,11 +1,9 @@
 use std::{env, process};
-
-use minigrep::{run, Config};
+mod library;
+use library::{run, Config};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: '{}'", err);
         process::exit(1);
     });
