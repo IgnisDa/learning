1e54a3e1875c12a2d38e6a75ea19e1e14a348cee
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-04-10 23:28:25 +0550

    feat(Toggler.vue): add toggler component

---
+++ ui-ux-practice/password-generator/components/Toggler.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="flex items-center h-4 p-1 bg-indigo-500 cursor-pointer w-7 dark:bg-indigo-700 sm:w-12 md:w-14 sm:h-6 md:h-8 rounded-3xl"
+  >
+    <div
+      class="w-3 h-3 transition-transform duration-300 ease-in-out transform bg-gray-100 shadow-xl rounded-3xl sm:w-4 md:w-6 sm:h-4 md:h-6 ring-1 ring-red-300"
+      :class="{
+        'translate-x-full': toggled,
+      }"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    toggled: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
