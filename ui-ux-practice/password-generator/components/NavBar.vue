a1215ed02da661caece9ba5cb329d311d7ff662f
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-04-10 13:06:22 +0550

    feat(NavBar.vue): add color mode toggler

--- ui-ux-practice/password-generator/components/NavBar.vue
+++ ui-ux-practice/password-generator/components/NavBar.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="z-10 flex p-2 shadow-2xl bg-cyan-600 dark:bg-fuchsia-600">
-    <ig-icon
-      name="moon"
-      class="w-6 h-6 cursor-pointer sm:h-8 sm:w-8 md:w-10 md:h-10 lg:w-14 lg:h-14"
-      variant="black"
-    ></ig-icon>
-    <ig-icon
-      name="sun"
-      class="w-6 h-6 cursor-pointer sm:h-8 sm:w-8 md:w-10 md:h-10 lg:w-14 lg:h-14"
-      variant="black"
-    ></ig-icon>
+  <div class="flex justify-center">
+    <div
+      class="z-10 flex items-center p-2 mt-3 space-x-3 rounded-lg shadow-2xl dark:bg-fuchsia-600"
+    >
+      <ig-icon
+        name="moon"
+        class="w-6 h-6 cursor-pointer sm:h-8 sm:w-8 md:w-10 md:h-10"
+        no-size
+        no-color
+      ></ig-icon>
+      <div
+        class="flex items-center h-4 p-1 bg-indigo-500 cursor-pointer w-7 dark:bg-indigo-700 sm:w-12 md:w-14 sm:h-6 md:h-8 rounded-3xl"
+        @click="toggleColorMode()"
+      >
+        <div
+          class="w-3 h-3 transition-transform duration-300 ease-in-out transform bg-gray-100 shadow-xl rounded-3xl sm:w-4 md:w-6 sm:h-4 md:h-6 ring-1 ring-red-300"
+          :class="{
+            'translate-x-full': $colorMode.preference === 'light',
+          }"
+        ></div>
+      </div>
+      <ig-icon
+        name="sun"
+        class="w-6 h-6 cursor-pointer sm:h-8 sm:w-8 md:w-10 md:h-10"
+        no-size
+        no-color
+      ></ig-icon>
+    </div>
   </div>
 </template>
 
@@ -19,15 +36,7 @@ export default {
   data: () => ({
     clicked: true,
   }),
-  computed: {
-    getColorModeIcon() {
-      if (this.$colorMode.value === 'light') {
-        return 'LightModeIcon'
-      } else {
-        return 'darkModeIcon'
-      }
-    },
-  },
+  computed: {},
   methods: {
     toggleColorMode() {
       if (this.$colorMode.value === 'light') {
