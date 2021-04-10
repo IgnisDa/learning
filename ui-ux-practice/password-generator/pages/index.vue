515e4a65947513e6d0a664dab81aab8a33a8a037
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-04-10 13:07:03 +0550

    feat(index.vue): add simple generator

--- ui-ux-practice/password-generator/pages/index.vue
+++ ui-ux-practice/password-generator/pages/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <div class="container text-5xl md:text-9xl font-extralight">Hello World</div>
+  <div class="flex items-center justify-center">
+    <div class="bg-red-100">
+      <div>{{ generatedPassword }}</div>
+      <div>{{ config }}</div>
+      <div>
+        <button
+          class="appearance-none"
+          @click="generateAndSetPasswordMutation()"
+        >
+          Generate
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      generatedPassword: (state) => state.password.generatedPassword,
+      config: (state) => state.password.config,
+    }),
+  },
+  mounted() {},
+  methods: {
+    ...mapMutations({
+      generateAndSetPasswordMutation: 'password/generateAndSetPassword',
+      togglePasswordIncludeUppercaseMutation:
+        'password/togglePasswordIncludeUppercase',
+    }),
+  },
+}
 </script>
