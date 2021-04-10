ae1b3150c1bcfe99583a91a4125996e96ff2a4c4
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-04-10 23:30:08 +0550

    feat(NavBar.vue): use toggler component

--- ui-ux-practice/password-generator/components/NavBar.vue
+++ ui-ux-practice/password-generator/components/NavBar.vue
@@ -9,17 +9,10 @@
         no-size
         no-color
       ></ig-icon>
-      <div
-        class="flex items-center h-4 p-1 bg-indigo-500 cursor-pointer w-7 dark:bg-indigo-700 sm:w-12 md:w-14 sm:h-6 md:h-8 rounded-3xl"
-        @click="toggleColorMode()"
-      >
-        <div
-          class="w-3 h-3 transition-transform duration-300 ease-in-out transform bg-gray-100 shadow-xl rounded-3xl sm:w-4 md:w-6 sm:h-4 md:h-6 ring-1 ring-red-300"
-          :class="{
-            'translate-x-full': $colorMode.preference === 'light',
-          }"
-        ></div>
-      </div>
+      <toggler
+        :toggled="$colorMode.preference === 'light'"
+        @click.native="toggleColorMode()"
+      ></toggler>
       <ig-icon
         name="sun"
         class="w-6 h-6 cursor-pointer sm:h-8 sm:w-8 md:w-10 md:h-10"
@@ -31,8 +24,9 @@
 </template>
 
 <script>
+import Toggler from './Toggler.vue'
 export default {
-  components: {},
+  components: { Toggler },
   data: () => ({
     clicked: true,
   }),
