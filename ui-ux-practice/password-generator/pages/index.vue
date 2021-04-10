d8624775bce53f1c81ac8a9bd4303fd858389e5c
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-04-10 23:28:46 +0550

    feat(index.vue): render all togglers

--- ui-ux-practice/password-generator/pages/index.vue
+++ ui-ux-practice/password-generator/pages/index.vue
@@ -2,7 +2,19 @@
   <div class="flex items-center justify-center">
     <div class="bg-red-100">
       <div>{{ generatedPassword }}</div>
-      <div>{{ config }}</div>
+      <div>
+        <div
+          v-for="(action, index) in actions"
+          :key="index"
+          class="flex items-center"
+        >
+          <div>{{ action.label }}</div>
+          <toggler
+            :toggled="$store.state.password[action.state]"
+            @click.native="action['action']()"
+          ></toggler>
+        </div>
+      </div>
       <div>
         <button
           class="appearance-none"
@@ -17,12 +29,40 @@
 
 <script>
 import { mapState, mapMutations } from 'vuex'
+import Toggler from '~/components/Toggler.vue'
 
 export default {
+  components: { Toggler },
+  data() {
+    return {
+      actions: [
+        {
+          label: 'Include Uppercase',
+          state: 'uppercase',
+          action: this.togglePasswordIncludeUppercaseMutation,
+        },
+        {
+          label: 'Include Lowercase',
+          state: 'lowercase',
+          action: this.togglePasswordIncludeLowercaseMutation,
+        },
+        {
+          label: 'Include Numbers',
+          state: 'numbers',
+          action: this.togglePasswordIncludeNumbersMutation,
+        },
+        {
+          label: 'Include Symbols',
+          state: 'symbols',
+          action: this.togglePasswordIncludeSymbolsMutation,
+        },
+      ],
+    }
+  },
   computed: {
     ...mapState({
       generatedPassword: (state) => state.password.generatedPassword,
-      config: (state) => state.password.config,
+      length: (state) => state.password.length,
     }),
   },
   mounted() {},
@@ -31,6 +71,13 @@ export default {
       generateAndSetPasswordMutation: 'password/generateAndSetPassword',
       togglePasswordIncludeUppercaseMutation:
         'password/togglePasswordIncludeUppercase',
+      togglePasswordIncludeLowercaseMutation:
+        'password/togglePasswordIncludeLowercase',
+      togglePasswordIncludeNumbersMutation:
+        'password/togglePasswordIncludeNumbers',
+      togglePasswordIncludeSymbolsMutation:
+        'password/togglePasswordIncludeSymbols',
+      setPasswordLengthMutation: 'password/setPasswordLength',
     }),
   },
 }
