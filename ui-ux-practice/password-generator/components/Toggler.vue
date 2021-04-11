922533ccd18e7096a22e76d4d687666f1d271c02
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-04-11 12:52:31 +0550

    fix(Toggler.vue): transition of toggler thumb

--- ui-ux-practice/password-generator/components/Toggler.vue
+++ ui-ux-practice/password-generator/components/Toggler.vue
@@ -1,12 +1,10 @@
 <template>
   <div
-    class="flex items-center h-4 p-1 bg-indigo-500 cursor-pointer w-7 dark:bg-indigo-700 sm:w-12 md:w-14 sm:h-6 md:h-8 rounded-3xl"
+    class="relative flex items-center h-4 px-0.5 py-1 bg-indigo-500 cursor-pointer w-7 dark:bg-indigo-700 sm:w-12 md:w-14 sm:h-6 md:h-8 rounded-3xl"
   >
     <div
-      class="w-3 h-3 transition-transform duration-300 ease-in-out transform bg-gray-100 shadow-xl rounded-3xl sm:w-4 md:w-6 sm:h-4 md:h-6 ring-1 ring-red-300"
-      :class="{
-        'translate-x-full': toggled,
-      }"
+      class="absolute w-3 h-3 transition-all duration-300 ease-in-out transform bg-gray-100 shadow-xl toggler rounded-3xl sm:w-4 md:w-6 sm:h-4 md:h-6 ring-1 sm:ring-2 ring-blue-300"
+      :class="[toggled ? 'left-3 sm:left-7' : 'left-1']"
     ></div>
   </div>
 </template>
