3fbc5e587b7de9ca180e83d402896988fb806b1d
{{cookiecutter.github_name}} <{{cookiecutter.github_email}}>
2021-04-11 10:31:03 +0550

    feat(GenerateButton.vue): button to generate password
    This component will trigger a mutation which 
    will generate the password while respecting the
    settings of the user.

---
+++ ui-ux-practice/password-generator/components/GenerateButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="py-3 text-sm font-semibold tracking-wider text-white uppercase rounded-lg appearance-none px-7 focus:outline-none bg-gradient-to-br from-cyan-700 to-purple-800 sm:text-base"
+  >
+    Generate Password
+  </button>
+</template>
