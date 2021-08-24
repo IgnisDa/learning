70caf9d8827c1308e6b03d473952943d40cab8b3
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-08-24 08:19:48 +0000

    lot of changes

---
+++ ui-ux-practice/car-manager/frontend/src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { initClient } from '@urql/svelte';
+	initClient({
+		url: 'http://localhost:8000/graphql'
+	});
+</script>
+
+<slot />
---
+++ ui-ux-practice/car-manager/frontend/src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { operationStore, query } from '@urql/svelte';
+	const carsOperation = operationStore(`
+		query {
+			cars {
+				id
+				name
+			}
+		}
+		`);
+	const cars = query(carsOperation);
+</script>
+
+Prerender this page
+{JSON.stringify($cars.data.cars)}
