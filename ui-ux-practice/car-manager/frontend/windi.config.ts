4b4ec972f15ef7f910b3d89b610f5089f73a2755
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-08-26 20:26:52 +0550

    build(windi.config.ts): load new fonts

--- ui-ux-practice/car-manager/frontend/windi.config.ts
+++ ui-ux-practice/car-manager/frontend/windi.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'windicss/helpers';
+import defaultTheme from 'windicss/defaultTheme';
 
 const PRODUCTION = process.env.NODE_ENV !== 'production';
 
@@ -9,6 +10,14 @@ export default defineConfig({
 	},
 	darkMode: 'class',
 	presets: [require('frontend-commons/tailwind/preset')],
+	theme: {
+		fontFamily: {
+			...defaultTheme.fontFamily,
+			sans: ['Biotif', ...defaultTheme.fontFamily.sans],
+			banner: ['Secular One', 'sans-serif'],
+			heading: ['Bogart', 'sans-serif']
+		}
+	},
 	shortcuts: {
 		'debug-screens': PRODUCTION
 			? 'before:bottom-0 before:left-0 before:fixed before:z-[2147483647] before:px-1 before:text-12px before:bg-black before:text-white before:shadow-xl @sm:before:content-["screen:sm"] @md:before:content-["screen:md"] @lg:before:content-["screen:lg"] @xl:before:content-["screen:xl"] @2xl:before:content-["screen:2xl"] <sm:before:content-["screen:none"]'
