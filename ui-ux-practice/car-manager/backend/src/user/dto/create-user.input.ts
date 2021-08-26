4632f3e47e63fb55ad6d39c151c6ddf3235e748b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-08-26 09:30:14 +0550

    feat(create-user.input.ts, password.validator.ts): add password validator

--- ui-ux-practice/car-manager/backend/src/user/dto/create-user.input.ts
+++ ui-ux-practice/car-manager/backend/src/user/dto/create-user.input.ts
@@ -1,13 +1,21 @@
 import { InputType } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsPasswordValid } from '../password.validator';
 
 @InputType({ description: 'Type to use while creating a new user' })
 export class CreateUserInput {
   /* The username of the new user */
+  @IsNotEmpty()
   username: string;
 
   /* The email of the new user */
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
   /* The password of the new user */
+  @IsNotEmpty()
+  @IsPasswordValid()
+  @MinLength(10)
   password: string;
 }
