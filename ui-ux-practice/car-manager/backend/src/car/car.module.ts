02523aa021fd004708ccfb572d675bdf8eba9138
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-08-31 23:05:51 +0550

    feat(car.controller.ts, car.module.ts): delete useless file

--- ui-ux-practice/car-manager/backend/src/car/car.module.ts
+++ ui-ux-practice/car-manager/backend/src/car/car.module.ts
@@ -4,9 +4,10 @@ import { Car } from './entities/car.entity';
 import { CarService } from './car.service';
 import { CarResolver } from './car.resolver';
 import { CarPicture } from './entities/car-picture.entity';
+import { CarOrigin } from './entities/car-origin.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Car, CarPicture])],
+  imports: [TypeOrmModule.forFeature([Car, CarPicture, CarOrigin])],
   providers: [CarService, CarResolver],
   controllers: [],
 })
