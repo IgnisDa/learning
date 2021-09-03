6125d945ba27d90d51a9b5bf8f11c7d28a26eebb
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-09-03 16:28:10 +0550

    feat(seed.ts): add seeding script

---
+++ ui-ux-practice/car-manager/backend/prisma/seed.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from '@prisma/client';
+import * as faker from 'faker';
+
+const prisma = new PrismaClient();
+
+function randomElement(arr: any[]) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function getNullOrData(data: unknown) {
+  if (Math.random() < 0.8) return data;
+  return null;
+}
+
+async function seed() {
+  const originCountries = [];
+  for (let i = 0; i < 100; i++) {
+    originCountries.push({ name: `${i}-${faker.address.country()}` });
+  }
+  await prisma.carOrigin.createMany({
+    data: originCountries,
+  });
+  const carOrigins = await prisma.carOrigin.findMany({ select: { id: true } });
+  const cars = [];
+  for (let i = 0; i < 1000; i++) {
+    cars.push({
+      milesPerGallon: getNullOrData(faker.datatype.float({ min: 10, max: 30 })),
+      cylinders: getNullOrData(faker.datatype.number({ min: 2, max: 8 })),
+      displacement: getNullOrData(faker.datatype.float({ min: 200, max: 500 })),
+      horsepower: getNullOrData(faker.datatype.number({ min: 100, max: 1500 })),
+      weight: getNullOrData(faker.datatype.float({ min: 300, max: 1500 })),
+      acceleration: faker.datatype.float({ min: 2, max: 12 }),
+      year: faker.datatype.number({ min: 1900, max: 2020 }),
+      model: getNullOrData(faker.vehicle.model()),
+      name: faker.vehicle.vehicle(),
+      originCountryId: randomElement(carOrigins).id,
+    });
+  }
+  await prisma.car.createMany({ data: cars });
+}
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
