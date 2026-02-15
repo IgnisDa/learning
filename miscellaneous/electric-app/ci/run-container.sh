170cc7847cba98000205ad1a53072b56b7def976
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-15 20:13:39 +0530

    fix: add BETTER_AUTH_SECRET environment variable in run-container.sh

--- miscellaneous/electric-app/ci/run-container.sh
+++ miscellaneous/electric-app/ci/run-container.sh
@@ -3,6 +3,7 @@ set -eu
 
 export ELECTRIC_PORT="3001"
 export ELECTRIC_INSECURE="true"
+export BETTER_AUTH_SECRET="postgresql_postgres_password_localhost_electric"
 
 frontend_command='PORT=3000 node .output/server/index.mjs'
 backend_command='/app/bin/entrypoint start'
