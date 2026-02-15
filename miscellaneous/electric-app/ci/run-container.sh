e5104a6d49a86cc2dca6013c1511a4a24a8e5c6b
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-15 20:15:13 +0530

    fix: add ELECTRIC_URL environment variable in run-container.sh

--- miscellaneous/electric-app/ci/run-container.sh
+++ miscellaneous/electric-app/ci/run-container.sh
@@ -3,6 +3,7 @@ set -eu
 
 export ELECTRIC_PORT="3001"
 export ELECTRIC_INSECURE="true"
+export ELECTRIC_URL="http://localhost:3001"
 export BETTER_AUTH_SECRET="postgresql_postgres_password_localhost_electric"
 
 backend_command="/app/bin/entrypoint start"
