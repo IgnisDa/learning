99036a85811c59e1ed33fdca36099c1447c2842b
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-15 20:14:01 +0530

    fix: standardize command variable assignments in run-container.sh

--- miscellaneous/electric-app/ci/run-container.sh
+++ miscellaneous/electric-app/ci/run-container.sh
@@ -5,8 +5,8 @@ export ELECTRIC_PORT="3001"
 export ELECTRIC_INSECURE="true"
 export BETTER_AUTH_SECRET="postgresql_postgres_password_localhost_electric"
 
-frontend_command='PORT=3000 node .output/server/index.mjs'
-backend_command='/app/bin/entrypoint start'
+backend_command="/app/bin/entrypoint start"
+frontend_command="node .output/server/index.mjs"
 
 exec concurrently --names frontend,backend --kill-others \
   "$frontend_command" \
