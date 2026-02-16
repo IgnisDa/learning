98e11fa0582d6018de7c4054bb10d3af20acbab8
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-16 19:38:29 +0530

    chore: more env var extraction

--- miscellaneous/zero-sample/src/lib/common.ts
+++ miscellaneous/zero-sample/src/lib/common.ts
@@ -1,6 +1,8 @@
 export const isProd = process.env.NODE_ENV === "production";
 
 export const databaseURL = process.env.DATABASE_URL || "";
-
 if (!databaseURL)
   throw new Error("DATABASE_URL (or ZERO_UPSTREAM_DB) is required");
+
+export const tmdbKey = process.env.TMDB_API_KEY;
+if (!tmdbKey) throw new Error("TMDB_API_KEY is required");
