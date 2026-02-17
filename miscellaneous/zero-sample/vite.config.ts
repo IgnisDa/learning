bd7505e696db2cf21b75c91afad4d4bf78f6d48f
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-17 16:19:00 +0530

    chore: make frontend completely client side

--- miscellaneous/zero-sample/vite.config.ts
+++ miscellaneous/zero-sample/vite.config.ts
@@ -2,15 +2,13 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  server: { port: 3000 },
+  server: { host: "0.0.0.0" },
   ssr: { noExternal: ["reshaped"] },
   plugins: [
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
-    tanstackStart({ srcDirectory: "src" }),
+    tanstackStart({ srcDirectory: "src", spa: { enabled: true } }),
     viteReact(),
-    nitro(),
   ],
 });
